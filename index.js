6a463ff46f828d82c4d75d508412c2875306364b
kashif <kashifshah5898@gmail.com>
2022-10-13 13:20:33 +0500

    background is changing as expected

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const toggle = () => {
   let isChecked = document.getElementById("toggleCheckBox").checked;
-  console.log("Clicked: ", isChecked);
+  console.log(isChecked);
+  if (isChecked) {
+    document.getElementById("body").style.backgroundColor = "#FFFFFF";
+  } else {
+    document.getElementById("body").style.backgroundColor = "#20222f";
+  }
 };
